3ded0cb66b0deb7ad6302b0a7db9dd74ab022626
lukasschueler <lukas@newconnexxions.de>
2021-08-23 22:05:49 +0200

    Next feature in trial

--- run.py
+++ run.py
@@ -263,7 +263,7 @@ if __name__ == '__main__':
     parser.add_argument('--ext_coeff', type=float, default=1.)
     parser.add_argument('--int_coeff', type=float, default=1.)
     parser.add_argument('--layernorm', type=int, default=0)
-    parser.add_argument('--feat_learning', type=str, default="vaesph",
+    parser.add_argument('--feat_learning', type=str, default="pix2pix",
                         choices=["none", "idf", "vaesph", "vaenonsph", "pix2pix"])
     
     parser.add_argument('--num_dynamics', type=int, default=1)
@@ -272,7 +272,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     
-    wandb.init(project="thesis", group = "Exploration_by_Curiosity", entity = "lukischueler", name ="Feature Extractor: vaesph", config = args)
+    wandb.init(project="thesis", group = "Exploration_by_Curiosity", entity = "lukischueler", name ="Feature Extractor: pix2pix", config = args)
     # , monitor_gym = True)
     
 
